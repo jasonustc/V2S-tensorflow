d91c4a007e2df2839b70861c9fcba63ca7cf35e7
xu shen <shenxuustc@gmail.com>
2017-03-05 21:55:08 -0500

    add feature packing code

--- feat_extract/get_list.py
+++ feat_extract/get_list.py
@@ -3,7 +3,7 @@
 #     File Name           :     get_list.py
 #     Created By          :     shenxu
 #     Creation Date       :     [2017-03-01 09:07]
-#     Last Modified       :     [2017-03-02 06:39]
+#     Last Modified       :     [2017-03-05 05:08]
 #     Description         :      
 #################################################################################
 import os
@@ -12,6 +12,9 @@ import sys
 sys.path.append('../hdf5_generator')
 from generate_nolabel import splitdata
 import json
+import numpy as np
+import struct
+import h5py
 
 def get_list(path, key):
     assert os.path.isdir(path)
@@ -38,7 +41,7 @@ def get_index_file_c3d(video_data_folder, video_folder, c3d_feat_folder, input_l
     of_output = open(output_prefix, 'w')
     for path in os.listdir(video_data_folder):
         video_data_path = video_data_folder + path
-        video_path = video_folder + os.path.basename(path).split('.')[0] + '.avi'
+        video_path = video_folder + os.path.basename(path).split('.')[0] + '.mp4'
         frame_data = json.load(open(video_data_path))
         frame_list = frame_data[0]
         num = 0
@@ -58,7 +61,45 @@ def get_index_file_c3d(video_data_folder, video_folder, c3d_feat_folder, input_l
     of_input.close()
     of_output.close()
 
+def get_binary_data_c3d(input_file, shape, dtype = np.float32):
+    with open(input_file, 'rb') as f:
+        num = struct.unpack("i", f.read(4))
+        ch = struct.unpack("i", f.read(4))
+        length = struct.unpack("i", f.read(4))
+        height = struct.unpack("i", f.read(4))
+        width = struct.unpack("i", f.read(4))
+        data = np.fromfile(f, dtype = dtype)
+        array = np.reshape(data, shape)
+        return array
+
+def merge_and_save_as_h5(feat_folder, feat_name, key):
+    assert os.path.isdir(feat_folder)
+    feats = os.listdir(feat_folder)
+    batch_data = []
+    for feat_file in feats:
+        if feat_file.endswith(feat_name):
+            frame_data = get_binary_data_c3d(os.path.join(feat_folder, feat_file), [1, 4096])
+            batch_data.append(frame_data)
+    all_data = np.vstack(batch_data)
+    h5_file = feat_folder[:feat_folder.rfind('/')] + feat_folder[feat_folder.rfind('/'):] +  \
+        '_c3d_' + feat_name + '.h5'
+    batch = h5py.File(h5_file, 'w')
+    batch[key] = all_data
+
+
 if __name__ == '__main__':
 #    splitdata('/disk_new/XuKS/YouTubeClips', 1200, 100)
-    get_index_file_c3d('/disk_new/shenxu/msvd_data/', '/disk_new/XuKS/YouTubeClips/',
-            '/disk_new/shenxu/msvd_feat_c3d/', 'c3d_input_list.txt', 'c3d_output_prefix.txt')
+#    get_index_file_c3d('/disk_2T/shenxu/msvd_data/', '/disk_2T/XuKS/YouTubeClips_convert/',
+#            '/disk_2T/shenxu/msvd_feat_c3d/', 'c3d_input_list.txt', 'c3d_output_prefix.txt')
+#    get_binary_data_c3d('/disk_2T/shenxu/msvd_feat_c3d/n_Z0-giaspE_270_278/000000.fc6-1', [1, 4096])
+    c3d_feat_folder = '/disk_2T/shenxu/msvd_feat_c3d/'
+    folders = os.listdir(c3d_feat_folder)
+    count = 0
+    for folder in folders:
+        if os.path.isdir(os.path.join(c3d_feat_folder, folder)):
+            merge_and_save_as_h5(os.path.join(c3d_feat_folder, folder), 'fc6-1', 'data')
+            merge_and_save_as_h5(os.path.join(c3d_feat_folder, folder), 'fc7-1', 'data')
+            count += 1
+            if count % 100 == 0:
+                print count
+    print count
--- hdf5_generator/trans_video_youtube.py
+++ hdf5_generator/trans_video_youtube.py
@@ -6,11 +6,14 @@ import unicodedata
 import glob
 import os
 
-path = '/disk_new/shenxu/'
-feature_folder = 'msvd_feat_vgg_batch/'
-feat_folder = '/disk_new/shenxu/msvd_feat_vgg/'
+feature_folder = '/disk_2T/shenxu/msvd_feat_vgg_c3d_batch/'
+vgg_feat_folder = '/disk_2T/shenxu/msvd_feat_vgg/'
+c3d_feat_folder = '/disk_2T/shenxu/msvd_feat_c3d/'
+vgg_feat_name =  'fc6'
+c3d_feat_name = 'fc6'
 
-def trans_video_youtube(datasplit_list, datasplit):
+def trans_video_youtube(datasplit_list, datasplit, vgg_feat_name,
+        c3d_feat_name):
     assert len(datasplit_list) > 0
     re = json.load(open('msvd2sent.json'))
     batch_size = 100
@@ -23,59 +26,60 @@ def trans_video_youtube(datasplit_list, datasplit):
     data = []
     label = []
     for ele in datasplit_list:
-        feat_file = feat_folder + ele + '.h5'
-        assert os.path.isfile(feat_file)
-        print feat_file
-        train_batch = h5py.File(feat_file)
+        vgg_feat_file = vgg_feat_folder + ele + '.h5'
+        c3d_feat_file = c3d_feat_folder + ele + '_c3d_' + c3d_feat_name + '-1.h5'
+        assert os.path.isfile(vgg_feat_file)
+        assert os.path.isfile(c3d_feat_file)
+        vgg_feat = np.array(h5py.File(vgg_feat_file)[vgg_feat_name])
+        c3d_feat = np.array(h5py.File(c3d_feat_file)['data'])
+        # to solve the number of frames mismatch between different videos
+        sample_data = np.zeros([n_length, 4096 * 2])
+        concat_feat = np.concatenate((vgg_feat, c3d_feat), axis = 1)
+        sample_data[:concat_feat.shape[0], :] = concat_feat
         video_name = ele
-        pdb.set_trace()
-        #pdb.set_trace()
         if video_name in re.keys():
+            print video_name, 'num_sen:', len(re[video_name])
             for xxx in re[video_name]:
                 #pdb.set_trace()
                 if len(xxx.split(' ')) < 35:
-                    pdb.set_trace()
                     fname.append(video_name)
-                    print unicodedata.normalize('NFKD', xxx).encode('ascii','ignore')
                     title.append(unicodedata.normalize('NFKD', xxx).encode('ascii','ignore'))
-                    data.append(train_batch['fc7'])
+                    data.append(sample_data)
                     ll = np.zeros([n_length]) - 1
-                    ll[len(train_batch['fc7'].shape[0] - 1)] = 0
+                    ll[concat_feat.shape[0] - 1] = 0
                     label.append(ll)
                     cnt += 1
                     if cnt == batch_size:
-                        batch = h5py.File(path+feature_folder+'/'+datasplit+str(initial)+'.h5','w')
+                        batch = h5py.File(feature_folder + datasplit + '{:06d}'.format(initial) + '.h5','w')
+                        data = np.array(data)
                         data = np.transpose(data,(1,0,2))
-                        batch['data'] = np.array(data)#np.zeros((n_length,batch_size,4096*2))
+                        batch['data'] = data #np.zeros((n_length,batch_size,4096*2))
                         fname = np.array(fname)
                         title = np.array(title)
                         batch['fname'] = fname
                         batch['title'] = title
 #                       batch['pos'] = np.zeros(batch_size)
-                        batch['label'] = np.transpose(np.array(label))#np.zeros((n_length,batch_size))
+                        batch['label'] = np.transpose(np.array(label)) # np.zeros((n_length,batch_size))
                         fname = []
                         title = []
                         label = []
                         data = []
                         cnt = 0
                         initial += 1
+        #### last batch ####
         if ele == datasplit_list[-1] and len(fname) > 0:
-            pdb.set_trace()
             while len(fname) < batch_size:
                 fname.append('')
                 title.append('')
-            batch = h5py.File(path+feature_folder+'/'+datasplit+str(initial)+'.h5','w')
-            batch['data'] = np.zeros((n_length,batch_size,4096))
-            batch['data'][:,:len(data),:] = np.transpose(np.array(data),(1,0,2))#np.zeros((n_length,batch_size,4096))
+            batch = h5py.File(feature_folder + datasplit + '{:06d}'.format(initial) + '.h5','w')
+            batch['data'] = np.zeros((n_length,batch_size,4096*2))
+            batch['data'][:,:len(data),:] = np.transpose(np.array(data),(1,0,2))#np.zeros((n_length,batch_size,4096*2))
             fname = np.array(fname)
             title = np.array(title)
             batch['fname'] = fname
             batch['title'] = title
             batch['label'] = np.ones((n_length,batch_size))*(-1)
-            pdb.set_trace()
-            batch['label'][:,:len(data)] = np.array(label).T
-#            batch['pos'] = np.zeros(batch_size)
-#            batch['label'] = np.zeros((n_length,batch_size))
+            batch['label'][:,:len(data)] = np.array(label).T #np.zeros((n_length,batch_size))
 
 
 
@@ -89,13 +93,10 @@ def getlist(feature_folder_name, split):
 
 if __name__ == '__main__':
     dataset = np.load('msvd_dataset.npz')
-    trans_video_youtube(dataset['train'], 'train')
-    pdb.set_trace()
-    trans_video_youtube(dataset['val'], 'val')
-    pdb.set_trace()
-    trans_video_youtube(dataset['test'], 'test')
-    pdb.set_trace()
-    getlist(feature_folder,'train')
-    getlist(feature_folder,'val')
-    getlist(feature_folder,'test')
+    trans_video_youtube(dataset['train'], 'train', vgg_feat_name, c3d_feat_name)
+    trans_video_youtube(dataset['val'], 'val', vgg_feat_name, c3d_feat_name)
+    trans_video_youtube(dataset['test'], 'test', vgg_feat_name, c3d_feat_name)
+#    getlist(feature_folder,'train')
+#    getlist(feature_folder,'val')
+#    getlist(feature_folder,'test')
 
