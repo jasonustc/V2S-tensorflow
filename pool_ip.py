fix bug in video loss computation

pool_ip.py
@@ -200,10 +200,10 @@ class Video_Caption_Generator():
         ####### Semantic Mapping ########
 
         ####### Decoding ########
-        decode_image = tf.nn.xw_plus_b(output_semantic, self.decode_image_W, self.decode_image_b)
+        decode_image = tf.nn.xw_plus_b(output_semantic, self.decode_image_W, self.decode_image_b) # b x d
         ####### Decoding ########
 
-        return decode_image
+        return decode_image # b x d
 
 def train():
     assert os.path.isdir(home_folder)

utils/model_ops.py
@@ -228,9 +228,13 @@ def testing_all(sess, n_steps, ixtoword, caption_tf, name_tf):
 def test_all_videos(sess, n_steps, gt_video_tf, gen_video_tf):
     avg_loss = 0.
     for ind in xrange(n_steps):
-        gt_images, pd_image = sess.run([gt_video_tf, gen_video_tf])
-        gt_image = np.mean(gt_images, axis=1)
-        loss = np.sqrt(np.sum((pd_image - gt_image)**2, axis=1))
+        loss = 0.
+        gt_images, pd_images = sess.run([gt_video_tf, gen_video_tf]) # b x n x d
+        if len(pd_images.shape) == 2: # reconstruct mean_pooling
+            gt_images = np.mean(gt_images, axis=1) # b x d
+            loss = np.sqrt(np.sum((pd_image - gt_image)**2), axis=1)
+        else:
+            loss = np.sqrt(np.sum((pd_image - gt_image)**2), axis=(1,2))
         avg_loss += np.sum(loss) / gt_images.shape[0]
     return avg_loss / n_steps
 